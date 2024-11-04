6296a79d2dafb15d1814ce34b588ed22bbf2da46
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2024-11-04 14:29:23 +0100

    inital commit

---
+++ src/test/java/com/example/release_test/ReleaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.release_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
